082ded78f430fb7db471c70e88a4b3d6b979a44e
Cédric SAMARDZIJA <cedric.samardzija@gmail.com>
2022-08-23 23:38:31 +0200

    ajout des clefs

--- init.py
+++ init.py
@@ -1,2 +1,14 @@
 import krakenex
+import os
+
+#appel de l'api via krakenex
 kraken = krakenex.API()
+
+#chargement clé privé et clé api
+api_key = os.environ['API_KEY_KRAKEN']
+api_sec = os.environ['API_SEC_KRAKEN']
+tmp = os.environ['TMP']
+with open(tmp+'\\keys', "w") as f:
+    f.write(api_key+"\n"+api_sec)
+kraken.load_key(tmp+'\\keys')
+os.remove(tmp+'\\keys')
