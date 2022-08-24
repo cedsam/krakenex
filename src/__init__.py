202e65e0dd690c979b00c4b82b06d8f188b72db9
Cédric SAMARDZIJA <cedric.samardzija@gmail.com>
2022-08-24 22:02:33 +0200

    reclassement

--- src/__init__.py
+++ src/__init__.py
@@ -1,23 +1,27 @@
-import krakenex
-import os
+def main():
+    import krakenex
+    import os
 
-#appel de l'api via krakenex
-kraken = krakenex.API()
+    #appel de l'api via krakenex
+    kraken = krakenex.API()
 
-#chargement clé privé et clé api
-api_key = os.environ['API_KEY_KRAKEN']
-api_sec = os.environ['API_SEC_KRAKEN']
-tmp = os.environ['TMP']
-try:
-    os.remove(tmp+'\\keys')
-except OSError:
-    pass
+    #chargement clé privé et clé api
+    api_key = os.environ['API_KEY_KRAKEN']
+    api_sec = os.environ['API_SEC_KRAKEN']
+    tmp = os.environ['TMP']
+    try:
+        os.remove(tmp+'\\keys')
+    except OSError:
+        pass
 
-with open(tmp+'\\keys', "w") as f:
-    f.write(api_key+"\n"+api_sec)
-kraken.load_key(tmp+'\\keys')
+    with open(tmp+'\\keys', "w") as f:
+        f.write(api_key+"\n"+api_sec)
+    kraken.load_key(tmp+'\\keys')
 
-#récuperation du solde
-soldeEuro = kraken.query_private("Balance")
-soldeEuro = soldeEuro['result']
-soldeEuro = soldeEuro['ZEUR']
+    #récuperation du solde
+    soldeEuro = kraken.query_private("Balance")    
+    soldeEuro = soldeEuro['result']
+    soldeEuro = soldeEuro['ZEUR']
+
+if __name__ == "__main__":
+    main()
