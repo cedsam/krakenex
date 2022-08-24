51ab071aeef4c956d0d410e4fc5c8b0e109eadd8
Cédric SAMARDZIJA <cedric.samardzija@gmail.com>
2022-08-24 10:53:36 +0200

    ajout solde euro, suppression fichier keys dès le début

--- init.py
+++ init.py
@@ -1,5 +1,6 @@
 import krakenex
 import os
+import re
 
 #appel de l'api via krakenex
 kraken = krakenex.API()
@@ -8,9 +9,16 @@ kraken = krakenex.API()
 api_key = os.environ['API_KEY_KRAKEN']
 api_sec = os.environ['API_SEC_KRAKEN']
 tmp = os.environ['TMP']
+try:
+    os.remove(tmp+'\\keys')
+except OSError:
+    pass
+
 with open(tmp+'\\keys', "w") as f:
     f.write(api_key+"\n"+api_sec)
 kraken.load_key(tmp+'\\keys')
-os.remove(tmp+'\\keys')
 
-balance = kraken.query_private("Balance")
+#récuperation du solde
+soldeEuro = kraken.query_private("Balance")
+soldeEuro = soldeEuro['result']
+soldeEuro = soldeEuro['ZEUR']
