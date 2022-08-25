e3a6e0a46306f4c420e4c17a79562da705ef0bc5
Cédric SAMARDZIJA <cedric.samardzija@gmail.com>
2022-08-25 11:36:02 +0200

    ajout alertes mail avec id de connexion

--- src/__init__.py
+++ src/__init__.py
@@ -2,6 +2,7 @@ def main():
     import krakenex
     import os
     from modules import keyFile
+    from modules import mail
     import logging
     import keyring
 
@@ -9,6 +10,13 @@ def main():
     os.remove('krakenapi.log')
     logging.basicConfig(format='%(asctime)s %(levelname)-8s %(message)s', filename='krakenapi.log', encoding='utf-8', level=logging.DEBUG, datefmt='%d-%m-%Y %H:%M:%S')
     
+    #configuration mail
+    smtpUser = keyring.get_password('orange.username', 'username')
+    smtpPassword = keyring.get_password('smtp.orange.fr', 'password')
+    smtpServer = 'smtp.orange.fr'
+    smtpPort = 587
+    destination = keyring.get_password('imap.gmail.com', 'username')
+
     #appel de l'api via krakenex
     kraken = krakenex.API()
     logging.info(kraken)
@@ -20,8 +28,16 @@ def main():
     kraken.load_key(keys)
 
     #récuperation du solde
-    soldeEuro = kraken.query_private("Balance")
+    try:
+        soldeEuro = kraken.query_private("Balance")
+    except:
+        logging.warning("vérifier la connexion ou les clés API")
+        subject='Krakenapi warning'
+        msg='vérifier la connexion ou les clés API\nAdresse journaux :'
+        mail (smtpUser,smtpPassword,smtpServer,smtpPort,subject,msg,destination)
+        exit(1)
     soldeEuro = soldeEuro['result']
     soldeEuro = soldeEuro['ZEUR']
+
 if __name__ == "__main__":
     main()
