10021332b38e935b79e50384c26b80e6071ff9f7
Cédric SAMARDZIJA <cedric.samardzija@gmail.com>
2022-08-25 10:43:47 +0200

    ajout fonction journal

--- src/__init__.py
+++ src/__init__.py
@@ -2,16 +2,27 @@ def main():
     import krakenex
     import os
     from modules import keyFile
+    import logging
+
+    #configuration journaux
+    os.remove('krakenapi.log')
+    logging.basicConfig(format='%(asctime)s %(levelname)-8s %(message)s', filename='krakenapi.log', encoding='utf-8', level=logging.DEBUG, datefmt='%d-%m-%Y %H:%M:%S')
+    
     #appel de l'api via krakenex
     kraken = krakenex.API()
-    
-    #chargement clé privé et clé api
+    logging.info(kraken)
+
+    ##chargement clé privé et clé api
     api_key = os.environ['API_KEY_KRAKEN']
     api_sec = os.environ['API_SEC_KRAKEN']
     keys = keyFile(api_key,api_sec)
     kraken.load_key(keys)
-    #récuperation du solde
+
+    ##récuperation du solde
     soldeEuro = kraken.query_private("Balance")
+    if soldeEuro['error'] != '[]':
+        logging.warning("vérifier la connexion et les clés api")
+        exit(1)
     soldeEuro = soldeEuro['result']
     soldeEuro = soldeEuro['ZEUR']
 
